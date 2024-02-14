6b8a7c5f74d86c29fec60c570a971389a5fd2b53
Ninodev14 <115539009+Ninodev14@users.noreply.github.com>
2024-02-14 09:03:50 +0100

    initialisation new projet Cv

--- element/js/main.js
+++
@@ -1,8 +0,0 @@
-
-
-window.onload = function ()  { 
-            setTimeout(() => {
-                document.querySelector('.start').style.textShadow = "0px 40px 0px #D2AEFF,0px 80px 0px #9846FF,0 0 4px #9846FF,0 0 7px #9846FF,0 0 10px #9846FF";
-            }, 500);
-        };
-        
