1a0379b01f0247a526b9cc5568736f5c89ea67fa
Ninodev14 <115539009+Ninodev14@users.noreply.github.com>
2024-02-22 12:08:06 +0100

    add annimation scroll

---
+++ src/js/aparition.js
@@ -0,0 +1,22 @@
+function handleScrollAppear() {
+    const elementsToAppear = document.querySelectorAll('.projet-list');
+
+    elementsToAppear.forEach(function(element) {
+        const position = element.getBoundingClientRect();
+
+        // Check if the element is in the viewport
+        if (position.top <= window.innerHeight * 0.90) {
+            element.style.opacity = 1;
+            element.style.transform = 'translateY(0)';
+        } else {
+            element.style.opacity = 0;
+            element.style.transform = 'translateY(60px)';
+        }
+    });
+}
+
+// Attach the function to the scroll event
+window.onscroll = handleScrollAppear;
+
+// Initial check on page load
+window.onload = handleScrollAppear;
